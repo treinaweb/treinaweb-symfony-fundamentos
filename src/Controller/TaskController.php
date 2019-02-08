66549fee82f55cfdf9b7ffd47313b205b12dc748
Elton Fonseca <elton869@gmail.com>
2019-02-08 20:23:36 +0000

    16 – Recebendo parâmetros via rota

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -3,17 +3,22 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 class TaskController 
 {
     /**
      * Lista as tarefas do sistema
-     * 
-     * @Route("/lista", name="lista")
      */
     public function index()
     {
-        return new Response("Está é minha primeira página");
+        return new Response("Retorna lista de tarefas");
+    }
+
+    /**
+     * Mostra tarefa especifica
+     */
+    public function show($id)
+    {
+        return new Response("Mostrando a tarefa " . $id);
     }
 }
