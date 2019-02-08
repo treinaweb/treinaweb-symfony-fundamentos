1ac1449ed37ed4a1961bd5126ae967c5fe89f10f
Elton Fonseca <elton869@gmail.com>
2019-02-08 19:55:53 +0000

    15 – Rotas via annotations

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -3,11 +3,14 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class TaskController 
 {
     /**
      * Lista as tarefas do sistema
+     * 
+     * @Route("/lista", name="lista")
      */
     public function index()
     {
