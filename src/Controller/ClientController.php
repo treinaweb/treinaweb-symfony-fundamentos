841af39160e3ba16fe0665498dd22342ff35aa57
Elton Fonseca <elton869@gmail.com>
2019-02-25 16:38:51 +0000

    45 - Obtendo parâmetros passados na URL

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -4,14 +4,21 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class ClientController extends AbstractController
 {
     /**
      * @Route("/client", name="client")
      */
-    public function index()
+    public function index(Request $request)
     {
+        // var_dump(
+        //     $request->query->all(),
+        //     $request->query->get("teste", "padrao"),
+        //     $request->query->get("pessoa")["idade"]
+        // );
+
         return $this->render('client/index.html.twig', [
             'controller_name' => 'ClientController',
         ]);
